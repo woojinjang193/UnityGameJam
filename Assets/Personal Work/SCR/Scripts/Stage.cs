38f1467fb1e7ea5a9a8ba8923ee606c651ac19b8
FeAnd <tcf1268@gmail.com>
2025-11-09 01:16:54 +0900

    fix

--- Assets/Personal Work/SCR/Scripts/Stage.cs
+++ Assets/Personal Work/SCR/Scripts/Stage.cs
@@ -3,13 +3,6 @@ using UnityEngine;
 public class Stage : MonoBehaviour
 {
     [SerializeField] private int stageNum;
-<<<<<<< Updated upstream
-    [SerializeField] private string stageName;
-    [SerializeField] private Transform respawnPoint;
-=======
     [SerializeField] public string stageName;
     [SerializeField] public Transform respawnPoint;
-
-
->>>>>>> Stashed changes
 }
