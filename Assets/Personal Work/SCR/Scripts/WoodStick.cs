8edc71be01b3c9c32b828280b9fcb09f96eaceef
FeAnd <tcf1268@gmail.com>
2025-11-09 23:41:06 +0900

    [Fix] 반투명 코드 추가

--- Assets/Personal Work/SCR/Scripts/WoodStick.cs
+++ Assets/Personal Work/SCR/Scripts/WoodStick.cs
@@ -1,8 +1,10 @@
+using System.Collections.Generic;
 using JetBrains.Annotations;
 using UnityEngine;
 
 public class WoodStick : MonoBehaviour
 {
+    [SerializeField] List<SpriteRenderer> renderers;
     [SerializeField] GameObject normalStick;
     [SerializeField] GameObject fireStick;
     private Animator animator;
@@ -33,5 +35,15 @@ public class WoodStick : MonoBehaviour
         fireStick.SetActive(false);
         gameObject.tag = "Box";
         animator.Play("Idle");
+    }
+
+    public void EchoStick()
+    {
+        foreach (SpriteRenderer sR in renderers)
+        {
+            Color es = sR.color;
+            es.a = 0.3f;
+            sR.color = es;
+        }
     }
 }
